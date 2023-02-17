7251c24742a1392b6e4711173d6a74b6b1e0b901
Brane12345 <olaniyivictorrex@gmail.com>
2023-02-17 05:54:28 -0800

    innitial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Single digits numbers of base 10
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
