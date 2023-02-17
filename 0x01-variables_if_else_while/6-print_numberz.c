877d9845ce922de19724c0ad8860c0c9a01aff1b
Brane12345 <olaniyivictorrex@gmail.com>
2023-02-17 06:52:36 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabets in reverse
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
