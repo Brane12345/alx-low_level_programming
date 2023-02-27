5cd22a6e0156f93e93c3ab1c3b22eba9100ab809
Brane12345 <olaniyivictorrex@gmail.com>
2023-02-27 10:23:51 -0800

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,10 +6,12 @@
  */
 void puts_half(char *str)
 {
-	int len = _strlen(str);
-	int start = (len - 1) / 2;
+	int i;
 
-	for (int i = start; i < len; i++)
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
