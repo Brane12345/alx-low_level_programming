dd51751b3755f19093811db1654eff8bc3754c8e
Brane12345 <olaniyivictorrex@gmail.com>
2023-02-27 09:46:19 -0800

    innitial commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	nt a = 0;
+
+	while (s[a])
+		a++;
+	while (a--)
+		_putchar(s[a]);
+	_putchar('\n');
+}
