9a16fe923e675e0ce5cce7ad838273d1897e16ee
Brane12345 <olaniyivictorrex@gmail.com>
2023-03-01 07:59:42 -0800

    innitial commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using rot13.
+ * @s: string to be encoded.
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+	int i;
+	char c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c = s[i];
+		if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
+			s[i] = c + 13;
+		else if ((c >= 'N' && c <= 'Z') || (c >= 'n' && c <= 'z'))
+			s[i] = c - 13;
+	}
+	return (s);
+}
