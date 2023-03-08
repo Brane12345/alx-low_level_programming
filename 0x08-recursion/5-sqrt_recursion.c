8daf51e9db3f5c72ddef6eca792eda5f610be654
Brane12345 <olaniyivictorrex@gmail.com>
2023-03-08 02:17:43 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,39 @@
 #include "main.h"
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 /**
- * _sqrt_recursion - computes the natural square root of a number
- * @n: the number to compute the square root of
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
-int _sqrt_recursion(int n)
+int find_sqrt(int num, int root)
 {
-	return (compute_sqrt(n, 1));
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
 }
 
 /**
- * compute_sqrt - helper function to compute the square root recursively
- * @n: the number to compute the square root of
- * @i: the current guess for the square root
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
-int compute_sqrt(int n, int i)
+int _sqrt_recursion(int n)
 {
+	int root = 0;
+
 	if (n < 0)
-	{
 		return (-1);
-	}
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	if (i * i == n)
-	{
-		return (i);
-	}
-	if (i * i > n)
-	{
-		return (-1);
-	}
-	return (compute_sqrt(n, i + 1));
+
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
 }
