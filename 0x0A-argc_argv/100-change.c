51f528689af6d27f5869cb8229481561747e826a
Brane12345 <olaniyivictorrex@gmail.com>
2023-03-10 07:11:44 -0800

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the minimum number of coins to make change
- *        for an amount of money
- *
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 if successful, 1 if error
+ * count_coins - Counts the number of coins needed
+ *	for a certain amount of money
+ * @cents: The amount of money in cents
+ * Return: The number of coins needed
  */
-int main(int argc, char **argv)
+int count_coins(int cents)
 {
-	int cents, coins = 0;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+	int coins = 0;
 
 	while (cents >= 25)
 	{
@@ -56,6 +39,36 @@ int main(int argc, char **argv)
 		coins++;
 		cents -= 1;
 	}
+	return (coins);
+}
+
+/**
+ * main - Prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: The number of arguments passed to the program
+ * @argv: An array containing the arguments passed to the program
+ *
+ * Return: 0 if successful, 1 if an error occurred
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins = count_coins(cents);
 
 	printf("%d\n", coins);
 	return (0);
